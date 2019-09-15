4e8f26342b8fb79d8fae758b52c8d4574591fcef
Ralph Giles <giles@thaumas.net>
2019-09-14 22:44:35 -0700

    Rough script to generate a coptic layout.
    Use python to map the characters to the unicode escapes required
    but the xkb symbol loader. Passing non-ascii utf-8 causes crashes.

---
+++ coptic.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# Generate an xkb symbol file for Coptic.
+# Based on the soft keyboard at https://coptic-dictionary.org/
+
+# xkb symbol file header.
+print('default  partial alphanumeric_keys modifier_keys')
+print('xkb_symbols "basic" {')
+print()
+print('   name[Group1]= "Coptic";')
+
+# Numbers like a normal US qwerty layout.
+print('''
+    key <TLDE> {	[     grave,	asciitilde	]	};
+    key <AE01> {	[	  1,	exclam 		]	};
+    key <AE02> {	[	  2,	at		]	};
+    key <AE03> {	[	  3,	numbersign	]	};
+    key <AE04> {	[	  4,	dollar		]	};
+    key <AE05> {	[	  5,	percent		]	};
+    key <AE06> {	[	  6,	asciicircum	]	};
+    key <AE07> {	[	  7,	ampersand	]	};
+    key <AE08> {	[	  8,	asterisk	]	};
+    key <AE09> {	[	  9,	parenleft	]	};
+    key <AE10> {	[	  0,	parenright	]	};
+    key <AE11> {	[     minus,	underscore	]	};
+    key <AE12> {	[     equal,	plus		]	};
+''')
+
+row1 = ['ⲑ', 'ⲱ', 'ⲉ', 'ⲣ', 'ⲧ', 'ⲯ', 'ⲩ', 'ⲓ', 'ⲟ', 'ⲡ', 'ϣ']
+for i, c in enumerate(row1):
+    lower = f'U{ord(c):04X}'
+    upper = f'U{ord(c) - 1:04X}'
+    print(f'    key <AD{i:02d}> {{ [ {lower}, {upper} ] }};')
+print('    key <AD12> {	[ bracketleft,	bracketright	]	};')
+
+row2 = ['ⲁ', 'ⲥ', 'ⲇ', 'ϥ', 'ⲅ', 'ϩ', 'ϫ', 'ⲕ', 'ⲗ', 'ϧ', 'ⳉ']
+for i, c in enumerate(row2):
+    lower = f'U{ord(c):04X}'
+    upper = f'U{ord(c) - 1:04X}'
+    print(f'    key <AC{i:02d}> {{ [ {lower}, {upper} ] }};')
+
+row3 = ['ⲍ', 'ⲝ', 'ⲭ', 'ⲫ', 'ⲃ', 'ⲛ', 'ⲙ', 'ϭ', 'ϯ', 'ⲏ']
+for i, c in enumerate(row2):
+    lower = f'U{ord(c):04X}'
+    upper = f'U{ord(c) - 1:04X}'
+    print(f'    key <AB{i:02d}> {{ [ {lower}, {upper} ] }};')
+
+print('''
+    key <BKSL> {	[ backslash,         bar	]	};
+''')
+print('};')
